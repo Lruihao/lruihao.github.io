952f4282e35e5f07e2ba8b5b1fdaac514fd05fd6
Lruihao <1024@lruihao.cn>
2019-11-26 20:23:55 +0800

    《博採眾長》源码更新：2019/11/26 周二20:23:54.94

---
+++ themes/next/source/js/src/pageQRcode.js
@@ -0,0 +1,24 @@
+let userAgentInfo = navigator.userAgent;
+let Agents = ["Android", "iPhone",
+            "SymbianOS", "Windows Phone",
+            "iPad", "iPod"];
+let flag = true;
+for (let v = 0; v < Agents.length; v++) {
+    if (userAgentInfo.indexOf(Agents[v]) > 0) {
+        flag = false;
+        break;
+    }
+}
+if(flag){
+  let qrBox = document.createElement("div");
+  let qrImg = document.createElement("img");
+  let span = document.createElement("span");
+  let qrApi = `https://api.qrserver.com/v1/create-qr-code/?size=80x80&data=${window.location.href}`;
+  qrImg.src = qrApi;
+  qrImg.alt = "页面二维码";
+  span.textContent = "扫一扫在手机上阅读，谢谢分享！";
+  qrBox.appendChild(qrImg);
+  qrBox.appendChild(span);
+  qrBox.className = "qr-box";
+  document.querySelector(".copyright-box").appendChild(qrBox);
+}
