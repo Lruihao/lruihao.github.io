965176071b4b7daded2caf8716bd8d8486c3643a
Lruihao <1024@lruihao.cn>
2019-12-06 21:04:01 +0800

    《菠菜眾長》源码更新：2019/12/06 周五21:04:01.76

--- push_all.bat
+++ push_all.bat
@@ -1,6 +1,6 @@
 @echo off
 title 备份与部署
 git add .
-git commit -m "《博採眾長》源码更新：%date%%time%"
+git commit -m "《菠菜眾長》源码更新：%date%%time%"
 git push
 hexo clean&&hexo g -d
--- push_hexo.bat
+++ push_hexo.bat
@@ -1,5 +1,5 @@
 @echo off
 title 源码备份
 git add .
-git commit -m "《博採眾長》源码更新：%date%%time%"
+git commit -m "《菠菜眾長》源码更新：%date%%time%"
 git push
